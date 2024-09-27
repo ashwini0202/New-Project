540eb806893ef0b0a6cff12d60d2ae870c3367f0
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-27 19:26:58 +0200

    Adding file

---
+++ src/pack1/Test1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Test1 {
+    public static void main(String[] args) {
+      //  ArrayList <String> Al= new ArrayList<>();
+        HashMap <String, Integer> hm= new HashMap<>();
+        hm.put("Anjali",11);
+        hm.put("Ashwini",12);
+        hm.put("Akash",13);
+        System.out.println(hm);
+    }
+}
